6b86b31ccf3e2c53a76489d741c937f5c40416ee
nomanmkhan <nomanmuhammadkhan@gmail.com>
2021-02-04 18:08:29 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+// mock data 
+import data from './data.json';
 
+// components
+import Header from './components/Header';
+import ToDoList from './ToDoList';
+import ToDoForm from './ToDoForm';
+
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+const [toDoList, setToDoList] = useState(data);
+
+        // for checking and unchecking
+        const handleToggle = (id) => {
+        let mapped = toDoList.map(task => {
+          return task.id === Number(id) ? { ...task, complete: !task.complete } : { ...task};
+        });
+        setToDoList(mapped);
+      }
+    //   DELETING 
+        const handleFilter = () => {
+        let filtered = toDoList.filter(task => {
+          return !task.complete;
+        });
+        setToDoList(filtered);
+      }
+
+    // ADDING
+    const addTask = (userInput) => {
+        let copy = [...toDoList];
+        copy.push({id: toDoList.length + 1, task: userInput, complete: false });
+        setToDoList(copy);
+      }
+
+
+ return (
+   <div className="App">
+       <Header />
+       <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
+       <ToDoForm addTask={addTask}/>
+
+       
+   </div>
+ );
+}
+ 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// //mock data
+// import data from "./data.json";
+// //components
+// import Header from "./Header";
+// import ToDoList from "./ToDoList";
+// import ToDoForm from './ToDoForm';
+
+// function App() {
+  
+//   const [ toDoList, setToDoList ] = useState(data);
+
+//   const handleToggle = (id) => {
+//     let mapped = toDoList.map(task => {
+//       return task.id === Number(id) ? { ...task, complete: !task.complete } : { ...task};
+//     });
+//     setToDoList(mapped);
+//   }
+
+//   const handleFilter = () => {
+//     let filtered = toDoList.filter(task => {
+//       return !task.complete;
+//     });
+//     setToDoList(filtered);
+//   }
+
+//   const addTask = (userInput ) => {
+//     let copy = [...toDoList];
+//     copy = [...copy, { id: toDoList.length + 1, task: userInput, complete: false }];
+//     setToDoList(copy);
+//   }
+
+//   return (
+//     <div className="App">
+//       <Header />
+//       <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
+//       <ToDoForm addTask={addTask}/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
